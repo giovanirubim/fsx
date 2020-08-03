5eb1d1b742da18c77bfb936c48eac930af6a4554
Giovani Rubim <giovanirubim.f@gmail.com>
2020-08-03 16:12:33 -0300

    Alias; Recursive flag; root as argument

--- fsx.js
+++ fsx.js
@@ -3,6 +3,7 @@ const exec = require('child_process').execSync;
 const accept = {};
 const reject = {};
 let cmd = '';
+let recursive = false;
 const explore = (dir) => {
 	if (accept.dir && !accept.dir.test(dir)) {
 		return 0;
@@ -20,7 +21,9 @@ const explore = (dir) => {
 			return;
 		}
 		if (lstat.isDirectory()) {
-			total += explore(path + '\\');
+			if (recursive) {
+				total += explore(path + '\\');
+			}
 			return;
 		}
 		if (accept.name && !accept.name.test(name)) {
@@ -53,28 +56,45 @@ const explore = (dir) => {
 	});
 	return total;
 };
-const argMap = {
-	'n': null,
-	'in': null,
-	'd': null,
-	'id': null,
-	'c': null,
-	'ic': null,
-	'r': null
+const argMap = {};
+const nArgArg = {};
+const addArg = (names, nArgs, def) => {
+	names.forEach(name => {
+		argMap[name] = def;
+		nArgArg[name] = nArgs;
+	});
 };
+addArg(['n', 'name'], 1, null);
+addArg(['in', 'ignore-name'], 1, null);
+addArg(['d', 'dir', 'directory'], 1, null);
+addArg(['id', 'ignore-dir', 'ignore-directory'], 1, null);
+addArg(['c', 'content'], 1, null);
+addArg(['ic', 'ignore-content'], 1, null);
+addArg(['r', 'run'], 1, null);
+addArg(['R', 'recursive'], 0, false);
 const { argv } = process;
+let root = null;
 for (let i=2; i<argv.length; ++i) {
 	let str = argv[i];
 	if (str[0] !== '-') {
-		console.log('Invalid argument ' + str);
-		process.exit(1);
+		if (root) {
+			console.log('Invalid argument ' + str);
+			process.exit(1);
+		} else {
+			root = str;
+			continue;
+		}
 	}
 	let name = str.substr(1);
 	if (!(name in argMap)) {
 		console.log('Invalid argument ' + str);
 		process.exit(1);
 	}
-	argMap[name] = argv[++i] || null;
+	if (nArgArg[name]) {
+		argMap[name] = argv[++i] || null;
+	} else {
+		argMap[name] = true;
+	}
 }
 const regexArg = (name) => {
 	let arg = (argMap[name]||'').trim();
@@ -104,4 +124,19 @@ reject.name = regexArg('in');
 accept.content = regexArg('c');
 reject.content = regexArg('ic');
 cmd = argMap.r;
-console.log(`${ explore(process.cwd() + '\\') } matches`);
+recursive = argMap.R;
+if (!root) {
+	root = process.cwd() + '\\';
+} else if (root.includes(':')) {
+	root = root.replace(/\\$/, '') + '\\';
+} else {
+	root = process.cwd() + `\\${ root.replace(/(^\\)|(\\$)/g, '') }\\`;
+	while (root.includes('\\.\\')) {
+		root = root.replace('\\.\\', '\\');
+	}
+	let regex = /\\[^\\]+\\\.\.\\/;
+	while (regex.test(root)) {
+		root = root.replace(regex, '\\');
+	}
+}
+console.log(`${ explore(root) } matches`);
